fca286882919d3868440f18e014c8a383d7bea04
Youri van de Geer <yourivandegeer123@gmail.com>
2021-09-22 03:19:32 +0200

    Opdracht 1 af

---
+++ TimYouri/week 1/rivercrossing/Main2.py
@@ -0,0 +1,92 @@
+import copy
+illegalStates = [
+    "CG|FW",
+    "GW|CF",
+    "FW|CG",
+    "F|CGW",
+    "CGW|F",
+    "CF|GW"
+]
+path = []
+
+def dfs (node, path=[]):
+
+    path = path + [node.getname()]
+
+    if is_goal(node.east):
+        print("Het is gelukt!")
+        return [path]
+
+    paths = []
+
+    for child in successors(node):
+
+        stringcheck = child.getname()
+        if stringcheck not in path:
+            newpaths = dfs(child, path)
+
+            for newpath in newpaths:
+                paths.append(newpath)
+
+    return paths
+
+def is_goal(node):
+    if "CFGW" in node:
+        return True
+
+def successors(node1):
+    neighbours = []
+    tw = copy.deepcopy(node1.west)
+    te = copy.deepcopy(node1.east)
+    f = "F"
+
+    if "F" in tw:
+        te = te + f
+        tw1 = tw.replace("F", "")
+        twfnode = node(tw1, te)
+        if isLegal(twfnode.getname()):
+            neighbours.append(twfnode)
+        for x in tw1:
+            if x != "F":
+                te1 = te + x
+                tw2 = tw1.replace(x, '')
+            addnode = node(tw2, te1)
+            if isLegal(addnode.getname()):
+                neighbours.append(addnode)
+        return neighbours
+
+    if "F" in te:
+        tw = tw + "F"
+        te2 = te.replace("F", '')
+        tefnode = node(tw, te2)
+        if isLegal(tefnode.getname()):
+            neighbours.append(tefnode)
+        for x in te2:
+            if x != "F":
+                tw3 = tw + x
+                te3 = te2.replace(x, '')
+            addnode1 = node(tw3, te3)
+            if isLegal(addnode1.getname()):
+                neighbours.append((addnode1))
+        return neighbours
+
+
+    return neighbours
+
+class node:
+    def __init__(self, west, east):
+        self.west = ''.join(sorted(west))
+        self.east = ''.join(sorted(east))
+
+
+
+    def getname(self):
+        return "" + self.west +"|" + self.east
+
+def isLegal(node):
+    if node in illegalStates:
+        return False
+    return True
+
+startnode = node("CFGW", "")
+print(dfs(startnode))
---
+++ TimYouri/week 1/rivercrossing/main.py
@@ -0,0 +1,66 @@
+stateRiver = ['FGCW', '']
+illegalStates = ['WG', 'GC']
+gameEnd = 0
+lastLegalState = ['dr', 'dr']
+
+visitedStates = []
+
+def MoveTo(object, lastLegalState=lastLegalState):
+  #if gameEnd == 0:
+        if "F" in object:
+            for x in object:
+                if stateRiver[0].find(x) != -1:
+                    stateRiver[0] = stateRiver[0].replace(x, '')
+                    stateRiver[1] = stateRiver[1]+x
+
+            if isEndState() == 1:
+                print('You son of a bitch, you did it!')
+                gameEnd = 1
+                print(stateRiver)
+                return
+
+            if isIllegal() == 1:
+                print("You cannot make move, MoveTo(" +object +"), it is illegal. game over")
+                gameEnd = 1
+                print('Your current state: ')
+                print(stateRiver)
+                print('Your last legal state: ')
+                print()
+                print(lastLegalState)
+                return
+            else:
+                lastLegalState = stateRiver
+            print(stateRiver)
+        else:
+            print("Moveto("+ object+ ") onmogelijk. Kan " + object + " niet verplaatsen zonder de boer.")
+
+def MoveFrom(object):
+    if "F" in object:
+        for x in object:
+            temp1 = stateRiver[0]
+            temp2 = stateRiver[1]
+
+            if temp2.find(x) != -1:
+                stateRiver[1] = temp2.replace(x, '')
+                stateRiver[0] = temp1+x
+        print(stateRiver)
+
+def isEndState():
+    endstate = "FGCW"
+    for x in endstate:
+        if stateRiver[1].find(x) == -1:
+            return 0
+    return 1
+
+def isIllegal():
+    for x in illegalStates:
+        if stateRiver[1].find(x) == -1:
+            return 1
+        if stateRiver[0].find(x) == -1:
+            return 1
+
+
+MoveTo("FWGC")
+MoveTo("G")
+MoveFrom("FWGC")
+MoveTo("FW")
